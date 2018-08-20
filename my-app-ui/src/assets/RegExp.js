2eedc1205060063ab10a582a24a9c5e3e68d396c
xuwenliu <1436731293@qq.com>
2018-08-20 17:58:59 +0800

    修改RegExp.js

--- my-app-ui/src/assets/RegExp.js
+++ my-app-ui/src/assets/RegExp.js
@@ -1,3 +1,6 @@
+/**
+ * create by xuwenliu 2018/8/20
+ */
 export default {
     account:/^[a-zA-Z][a-zA-Z0-9_]{4,15}$/,//帐号是否合法(字母开头，允许5-16字节，允许字母数字下划线)
     password:/[A-Za-z0-9_]{6,20}/,//6-20位字母数字下划线密码
